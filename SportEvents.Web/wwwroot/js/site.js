6a6d56a29779cf50e08e710377148b73f51d64bc
Dwiki Ikhwan <Dwiki.Ikhwan@unit4.com>
2025-04-24 03:54:33 +0700

    move javascript to site.js

--- SportEvents.Web/wwwroot/js/site.js
+++ SportEvents.Web/wwwroot/js/site.js
@@ -1,4 +1,45 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var lastClickedBtn;
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    window.setTimeout(function () {
+        $('.alert-success')
+            .fadeTo(500, 0)
+            .slideUp(500, function () {
+                $(this).remove();
+            });
+    }, 3000);
+
+    // Check for success message
+    var successMessage = '@(TempData["SuccessMessage"] ?? "")';
+
+    if (successMessage) {
+        // Set message content
+        $('#successMessage').text(successMessage);
+
+        // Show modal
+        $('#successModal').modal('show');
+
+        setTimeout(function () {
+            $('#successModal').modal('hide');
+        }, 3000);
+    }
+
+    $('.confirm').on('click', function () {
+        lastClickedBtn = $(this);                // Remember which button was clicked
+        $('#confirmationMessage').text($(this).data('message'));
+        $('#confirmationModal').modal('show');
+    });
+
+    $('#confirmButton').on('click', function () {
+        const $form = $('#mainForm'),
+            actionUrl = lastClickedBtn.attr('formaction'),
+            methodType = lastClickedBtn.attr('formmethod')
+                || $form.attr('method');
+
+        $form.attr('action', actionUrl)
+            .attr('method', methodType)
+            .submit();
+    });
+});
+
+
